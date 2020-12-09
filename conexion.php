74aea71ea744f0ae5f750e8aab6e7579029d4fc0
Moises David Bobadilla Lozano <mdlb.lobo@gmail.com>
2020-12-09 11:07:29 -0600

    Añadido conexion

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "Motherboardmod1997.";
+$dbname = "bienesraices";
+
+$conn = new mysqli($servername, $username, $password, $dbname) or die("No ha sido posible conectarse. ");
